e1299c84982d55e413732da8a1f995bc22b18b3c
Philip Cox <philip@yesodbuilder.defaultdomain>
2013-03-12 00:28:46 +0000

    Started the installation script

--- README.md
+++ README.md
@@ -2,3 +2,11 @@ Required:
 
     GHC
     Shelly: cabal install shelly shelly-extra
+
+    Also, worth noting to try stuff out in Shelly in ghci you must enabled overloaded strings:
+        
+        ghci -XOverloadedStrings
+        ... (in ghci) ...
+        Prelude> import Shelly
+        Prelude Shelly> shelly $ run "ls" []
+        
--- install.hs
+++ install.hs
@@ -1,6 +1,17 @@
 {-# LANGUAGE OverloadedStrings #-}
 
 import Shelly
+import System.Environment
+
+usage = "\
+    \This will nuke your existing .vimrc and files, to be\n\
+    \sure you really mean to do this add the parameter:\n\
+    \   --ireallymeanit\n\
+    \\n"
 
 main = shelly $ do
-    run "ls" []
+    args <- liftIO $ getArgs
+    case args of
+        s | elem "--ireallymeanit" s -> echo "of course you do"
+        otherwise -> echo usage
+        
