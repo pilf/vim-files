2c2e9cbb8d31546cfdaa7b189ad8e06d6e898020
Philip Cox <themany@pitofhappiness.com>
2013-10-20 13:53:09 +0000

    Added install script

---
+++ env/bash/install.sh
@@ -0,0 +1,11 @@
+#/bin/bash
+basedir=$(cd "$(dirname "$0")/../.."; pwd)
+echo "Using base dir: $basedir"
+ln -s "$basedir/.vimrc" $HOME
+ln -s "$basedir/vimfiles" $HOME
+cp $HOME/.bashrc $HOME/.bashrc~
+echo "source \"$basedir/env/bash/bashrc-std.sh.inc\"" >> $HOME/.bashrc
+if [[ ! -d $HOME/bin ]]; then
+    mkdir $HOME/bin
+fi
+cp "$basedir/env/bash/today" "$HOME/bin"
