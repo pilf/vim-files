7937a01ffd9d432de7bc357a4b17f291e411be9f
Philip Cox <philip@sphere37.com>
2015-08-09 13:37:04 +0100

    Bug fix

--- env/bash/scripts/container-named.sh
+++ env/bash/scripts/container-named.sh
@@ -9,9 +9,9 @@ cmd="docker ps --all"
 count=$(docker_match_count "$cmd" "$pattern")
 if [ "$count" -gt 1 ]; then
     echo "More than one possible container, found: " 1>&2
-    match_printf_docker_columns "$cmd" "$pattern" '%s (%s) %s\n' 2 1 4 1>&2
+    match_printf_docker_columns "$cmd" "$pattern" '%s (%s) %s\n' 2 1 5 1>&2
     echo "<<undecidable>>" 
 else
-    match_printf_docker_columns "$cmd" "$pattern" '%s\n' 1 1>&2 
+    match_printf_docker_columns "$cmd" "$pattern" '%s\n' 1
 fi
 
