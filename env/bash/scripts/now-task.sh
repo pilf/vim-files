2a1f64b87c9b596d3be2db596e34dd282b5687ac
Philip Cox <philip@sphere37.com>
2015-11-01 20:12:41 +0000

    Improved readme + can now input strings with spaces

---
+++ env/bash/scripts/now-task.sh
@@ -0,0 +1,12 @@
+if [ ! $(g workbench) ]; then
+    echo ".g.places doens't have a 'workbench' entry">&2
+    exit 1
+fi
+
+workbench_file=$(g workbench)
+if [ ! -f "$workbench_file" ]; then
+    echo "File defined by .g.places but either \"$workbench_file\" not a file or doesn't exist.">&2
+    exit 2
+fi
+
+cat $workbench_file | sed -e '/^>>TODOs/,$d' | sed -n "/^$(now -D)/,\$p" | sed -n '/^[TE].*-\s*$/p' | tail -n 1 
