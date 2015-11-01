6af930c9d1602a1062c8f018144da093c8179436
Philip Cox <pcox@opentable.com>
2015-11-01 17:59:08 +0000

    Made "all" optional

--- env/bash/scripts/container-named.sh
+++ env/bash/scripts/container-named.sh
@@ -3,8 +3,21 @@ DIR="${BASH_SOURCE%/*}"
 if [[ ! -d "$DIR" ]]; then DIR="$PWD"; fi
 . "$DIR/docker_columns.inc.sh"
 
+docker_opts=
+while getopts ":a" opt; do
+    case $opt in
+        a)
+            docker_opts="--all"
+            shift
+            ;;
+        \?)
+            echo "Invalid option -$OPTARG" >&2
+            ;;
+    esac
+done 
+
 pattern="$*"
-cmd="docker ps --all"
+cmd="docker ps $docker_opts" 
 
 count=$(docker_match_count "$cmd" "$pattern")
 if [ "$count" -gt 1 ]; then
