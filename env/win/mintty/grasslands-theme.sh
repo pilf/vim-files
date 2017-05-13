2908cd0428e3ccdcf26fedc7a941a194b770dc10
Philip Cox <philip@sphere37.com>
2017-05-13 16:28:21 +0100

    Quick start nice looking consoles on windows

---
+++ env/win/mintty/grasslands-theme.sh
@@ -0,0 +1,14 @@
+#!/bin/sh
+
+# grasslands theme is green/teal based
+# http://www.colourlovers.com/palette/4358035/Shadowy_Greens
+
+# cursor (powdered suger)
+echo -ne '\e]12;#F7FDFA\a'
+
+# foreground (sea harvest)
+echo -ne '\e]10;#B1D8CA\a'
+
+# background (gunmetal gray)
+echo -ne '\e]11;#292E37\a'
+
