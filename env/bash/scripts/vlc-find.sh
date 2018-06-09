3b5cca20930348376091484b9f9931c39d4edd70
Philip Cox <philip@sphere37.com>
2018-06-09 23:38:54 +0100

    More useful default options

--- env/bash/scripts/vlc-find.sh
+++ env/bash/scripts/vlc-find.sh
@@ -1,9 +1,10 @@
 #!/bin/sh
 
 # usage: vlc-find [find options]
-# example: vlc-find -type '*.mp4'
+# example: vlc-find 
+# example: vlc-find -a iname '*dvd*'
 
-find . -type f $@ \
+find . -type f -iname '*.mp4' -o -iname '*.avi' -o -iname '*.flv' -o -iname '*.mpeg' -o -iname '*.mpg' $@ \
     | sed -e 's#^.*cygdrive/c/\(.*\)$#c:/\1#' -e 's#\\#/#g' -e 's#\(.*\)#"\1"#' \
     | awk '{print} END {print "vlc://quit"}' \
     | tr '\n' ' ' \
