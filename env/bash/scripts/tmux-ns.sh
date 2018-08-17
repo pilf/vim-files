c8857e837562e2800b43440e89411f4ad03d1c92
Philip Cox <philip@sphere37.com>
2018-08-17 15:13:19 +0100

    Can now switch between tmux session easily

--- env/bash/scripts/tmux-ns.sh
+++ env/bash/scripts/tmux-ns.sh
@@ -1,6 +1,8 @@
 #!/bin/sh
 name="$1"
-tmux new-session -d bash
-tmux rename-session -t $(tmux list-session -F "#S" | grep '^[0-9]*$' | awk '{printf "%05d\n", $1}' | sort | head -n1 | sed s/^0*//) $name
-tmux-color.sh $name
-tmux switch -t $name
+if ! tmux has-session -t "$name" 2>/dev/null; then
+    tmux new-session -d bash 
+    tmux rename-session -t $(tmux list-session -F "#S" | grep '^[0-9]*$' | awk '{printf "%05d\n", $1}' | sort | head -n1 | sed s/^0*//) $name
+fi
+tmux-color.sh "$name"
+tmux switch -t "$name"
