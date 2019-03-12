f9195f5ed9f0c0cd4399c1530be87ec8aeeacc8e
Philip Cox <coxp@GLAdmins-MacBook-Pro-2.local>
2019-03-12 10:43:18 +0000

    Made it case insensitive

--- env/bash/scripts/find-in-files-egrep.sh
+++ env/bash/scripts/find-in-files-egrep.sh
@@ -20,4 +20,4 @@ regex="$1"
 namepattern="${2:-*}"
 path="${3:-$(pwd)}"
 
-find $path -type f -iname "$namepattern" -print0 | xargs -0 egrep "$regex"
+find $path -type f -iname "$namepattern" -print0 | xargs -0 egrep -i "$regex"
