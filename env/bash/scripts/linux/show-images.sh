e658190a1b2ba344a2cc675f0e1c94819faf82e3
Philip Cox <philip@sphere37.com>
2020-06-05 17:11:46 +0100

    show-images improvements

--- env/bash/scripts/linux/show-images.sh
+++ env/bash/scripts/linux/show-images.sh
@@ -1,2 +1,5 @@
-#!/bin/sh
-feh -FZ.r --info "echo %f -- %u/%l" -D -10.0
+#!/bin/bash
+set -x
+dur=${1:-10}
+cploc="${1:-$HOME/crypt/today}"
+feh -FZ.r --info "echo %f -- %u/%l" -D -$dur.0 --action1 "cp %F $cploc"
