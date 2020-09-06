fc278356b34bd8e2b079843f1ba2b1888e8c4150
Philip Cox <philip@sphere37.com>
2020-09-07 00:03:32 +0100

    Added random mode

--- env/bash/scripts/linux/show-images.sh
+++ env/bash/scripts/linux/show-images.sh
@@ -1,5 +1,18 @@
 #!/bin/bash
 set -x
 dur=${1:-10}
-cploc="${1:-$HOME/crypt/today}"
-feh -FZ.r --info "echo %f -- %u/%l" -D -$dur.0 --action1 "cp %F $cploc"
+mode=${2:seq}
+cploc="${2:-$HOME/crypt/today}"
+
+opts=""
+case $mode in
+    seq) 
+        ;;
+    r*) 
+        opts="-z"
+        ;;
+    *)
+        ;;
+esac
+
+feh -FZ.r --info "echo %f -- %u/%l" -D -$dur.0 --action1 "cp %F $cploc" $opts
